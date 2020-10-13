084be5e556602f1e25306ff2ce5137259d4511f6
Sohyo <cienakim12@gmail.com>
2020-10-13 11:05:10 +0200

    add get_split_doc.py : This file split the xmlfiles by preprocessing OPUS tool into 2 separate files(source/target)

---
+++ soyo/get_split_doc.py
@@ -0,0 +1,38 @@
+text_path = "/home/diego/PycharmProjects/my_fairseq/soyo/orig/EMEA_orig/xmlfiles_per_doc/9"
+
+# split the xml file into 2 plain text file(source/target)
+def split_into_src_trg(text_path):
+
+    source, target = [], []
+    src_text, trg_text = '', ''
+    start_point = ">"
+
+    with open(text_path) as f:
+        for line in f:
+            if line.startswith('(src)'):
+                if src_text == '':  # when it is the starting point of the sentence(or text)
+                    src_text = line[line.index(start_point) + len(start_point):][:-1]
+                else:   # When there are several pieces of sentences, join them with blanks
+                    src_text = ' '.join([src_text, line[line.index(start_point) + len(start_point):][:-1]])
+                # src_text += line[line.index(start_point) + len(start_point):][:-1]
+            elif line.startswith('(trg)'):
+                if trg_text == '':  # when it is the starting point of the sentence(or text)
+                    trg_text = line[line.index(start_point) + len(start_point):][:-1]
+                else:   # When there are several pieces of sentences, join them with blanks
+                    trg_text = ' '.join([src_text, line[line.index(start_point) + len(start_point):][:-1]])
+            elif line.startswith('==========='):    # finish saving the sentence as a block
+                source.append(src_text)
+                target.append(trg_text)
+                src_text = ''
+                trg_text = ''
+
+    return source[1:], target[1:]
+
+
+def arr2txt(arr, file_name):
+    with open("custom_data_per_doc/EMEA/"+file_name, "w") as txt_file:
+        for line in arr:
+            txt_file.write("".join(line) + "\n")
+
+# arr = text2arr(text_path)
+# print(arr)
